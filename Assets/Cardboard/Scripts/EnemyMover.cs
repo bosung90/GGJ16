505062951003f305db77db26b72d1992d0675d40
Lisa Wong <miss.lisa19@hotmail.com>
2016-01-30 21:09:11 -0800

    1. Spawning new enemies.

---
+++ Assets/Cardboard/Scripts/EnemyMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMover : MonoBehaviour {
+
+    public Vector3 velocity = new Vector3(0, 0, -4);
+
+	// Use this for initialization
+	void Start ()
+    {
+	    if (Cardboard.SDK.VRModeEnabled)
+        {
+            this.GetComponent<Rigidbody>().velocity = velocity;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
