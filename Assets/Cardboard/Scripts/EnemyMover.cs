5b95751f2755a8f972d4809bc8672e335c0bdf1c
Lisa Wong <miss.lisa19@hotmail.com>
2016-01-30 21:17:39 -0800

    1. Destroy enemy object when go out of game area. 2. Spawn enemy in random places.

--- Assets/Cardboard/Scripts/EnemyMover.cs
+++ Assets/Cardboard/Scripts/EnemyMover.cs
@@ -16,6 +16,7 @@ public class EnemyMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        if (transform.position.z < -20f)
+            Destroy(this.gameObject);
 	}
 }
