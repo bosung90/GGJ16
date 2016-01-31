11f0521119917189f595d49ce47f79bfc8064978
Lisa Wong <miss.lisa19@hotmail.com>
2016-01-30 22:38:37 -0800

    1. Objects to use, alchemy objects. 2. Shooting mushrooms.

---
+++ Assets/Cardboard/Scripts/RitualObjectController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualObjectController : MonoBehaviour {
+
+    float objSpeed = 9f;
+    float rotationSpeed = 8f;
+    private GameObject head;
+
+	// Use this for initialization
+	void Start () {
+        head = GameObject.FindGameObjectWithTag("Head");
+        this.GetComponent<Rigidbody>().velocity = head.transform.forward * objSpeed;
+        //this.GetComponent<Rigidbody>().angularVelocity = this.transform.forward * rotationSpeed;
+        Destroy(this.gameObject, 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Cardboard/Scripts/RitualObjectsLauncher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualObjectsLauncher : MonoBehaviour {
+
+    public RitualObjectController ritualObject;
+    private Vector3 _vrShooterOffset;
+
+	// Use this for initialization
+	void Start () {
+        _vrShooterOffset = new Vector3(0f, -0.4f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Cardboard.SDK.VRModeEnabled && Cardboard.SDK.Triggered)
+        {
+            GameObject vrLauncher = Cardboard.SDK.GetComponentInChildren<CardboardHead>().gameObject;
+            LaunchRitualObjectFrom(vrLauncher, _vrShooterOffset);
+        }
+	}
+
+    void LaunchRitualObjectFrom(GameObject origin, Vector3 shooterOffset)
+    {
+        Vector3 rotation = origin.transform.rotation.eulerAngles;
+        rotation.x = 90f;
+        Vector3 offset = origin.transform.rotation * shooterOffset;
+
+        Instantiate(ritualObject, origin.transform.position + offset, Quaternion.Euler(rotation));
+    }
+}
